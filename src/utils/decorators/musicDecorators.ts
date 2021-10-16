f43cdbd986808f3d76428fce729aca3f55fb1683
PoProstuWitold <witekzawada@o2.pl>
2021-10-16 21:09:55 +0200

    Music Decorators

---
+++ src/utils/decorators/musicDecorators.ts
@@ -0,0 +1,70 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Message } from 'discord.js';
+import { createMessageEmbed } from '../../utils/createEmbedMessage'
+import { Inhibit } from './inhibit'
+
+export function DoesMusicQueueExist(moreThanOne?: boolean): any {
+	return Inhibit((message: Message) => {
+		const queue = message.client.distube.getQueue(message)
+		if (!queue) return message.channel.send({
+			embeds: [ createMessageEmbed({ title: 'Error', description: 'There is nothing in the queue' })]
+		})
+
+		if(moreThanOne) {
+			if(message.client.distube.getQueue(message)!.songs.length < 2) {
+				return message.channel.send({
+					embeds: [ createMessageEmbed({ title: 'Error', description: 'There is nothing left in playlist' })]
+				})
+			} 
+		}
+	})
+}
+
+export function IsInVoiceChannel(): any {
+	return Inhibit((message: Message) => {
+		if (!message.member?.voice.channel) {
+			return message.channel.send({
+				embeds: [ createMessageEmbed({ title: 'Error', description: 'Please join VoiceChannel First!' })]
+			})
+		}
+	})
+}
+
+export function IsValidVoiceChannel(): any {
+	return Inhibit((message: Message) => {
+		const voiceChannel = message.member?.voice.channel
+		if (voiceChannel?.id === message.guild?.me?.voice.channel?.id) return undefined;
+		if (!voiceChannel?.joinable) {
+			return message.channel.send({ 
+				embeds: [ createMessageEmbed({ title: 'Error', 
+					description: 'Sorry, but I can\'t **`CONNECT`** to your voice channel, make sure I have the proper permissions.' 
+				}) ] })
+		}
+	})
+}
+
+export function ArgsNotEmpty(): any {
+	return Inhibit((message: Message, args: string[]) => {
+		if(!args[0]) {
+			return message.reply({
+				embeds: [
+					createMessageEmbed({ title: 'Error', description: 'Please add a Search Query!!!!!!!!' })
+				],
+			})			
+		}
+	})
+}
+
+// NOT WORKING CORRECTLY
+// export function IsBotSomewhereElse(): any {
+// 	return Inhibit((message: Message) => {
+// 		if(message.member?.voice.channel!.guild.me?.voice.channel && message.member?.voice.channel!.guild.me.voice.channel.id != message.channel!.id) {
+// 			return message.reply({
+// 				embeds: [
+// 					createMessageEmbed({ title: 'Error', description: 'I am already connected somewhere else!' })
+// 				]
+// 			})
+// 		}
+// 	})
+// }
