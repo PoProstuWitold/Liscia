1f6e89cd37d0d1238f2045c1390bb34e709c05b1
PoProstuWitold <witekzawada@o2.pl>
2021-10-17 22:23:48 +0200

    Procfile

---
+++ copyStaticAssets.ts
@@ -0,0 +1,5 @@
+import * as shell from 'shelljs';
+
+shell.cp('-R', 'src/dashboard/src/public/styles', 'dist/src/dashboard/src/public')
+shell.cp('-R', 'src/dashboard/src/public/images', 'dist/src/dashboard/src/public')
+shell.cp('-R', 'src/dashboard/views', 'dist/src/dashboard')
