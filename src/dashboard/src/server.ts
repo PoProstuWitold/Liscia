1dcbc45118215e9c4643e46df155482b3e1d18b4
PoProstuWitold <witekzawada@o2.pl>
2021-10-19 21:29:41 +0200

    host

--- src/dashboard/src/server.ts
+++ src/dashboard/src/server.ts
@@ -5,16 +5,19 @@ import https from 'https'
  * Start Express server.
  */
 
-const herokuUrl = 'https://liscia-elfrieden.herokuapp.com/'
+const herokuUrl = 'https://liscia-elfrieden.herokuapp.com'
+
+const HOST = process.env.HOST || herokuUrl
 
 const server = app.listen(app.get('port'), () => {
 
 	setInterval(() => {
+		console.log('Request received from setInterval')
 		https.get(herokuUrl)
 	}, 300000)
 
 	console.log(
-		`App is running at ${app.get('host')}:${app.get('port')}`,
+		`App is running at ${HOST}:${app.get('port')}`,
 		app.get('env')
 	)
 })
