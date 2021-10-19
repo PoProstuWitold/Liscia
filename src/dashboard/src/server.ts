1d6ef5df4633a5f0cb51f17ddedbe2583201f045
PoProstuWitold <witekzawada@o2.pl>
2021-10-19 21:17:16 +0200

    setInterval

--- src/dashboard/src/server.ts
+++ src/dashboard/src/server.ts
@@ -1,15 +1,22 @@
 import app from './app'
+import https from 'https'
 
 /**
  * Start Express server.
  */
+
+const herokuUrl = 'https://liscia-elfrieden.herokuapp.com/'
+
 const server = app.listen(app.get('port'), () => {
+
+	setInterval(() => {
+		https.get(herokuUrl)
+	}, 300000)
+
 	console.log(
-		'  App is running at http://localhost:%d in %s mode',
-		app.get('port'),
+		`App is running at ${app.get('host')}:${app.get('port')}`,
 		app.get('env')
 	)
-	console.log('  Press CTRL-C to stop\n')
 })
 
 export default server
