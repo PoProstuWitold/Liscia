7a0ad7c10ea745401e975fb58e52d758ef235e0c
PoProstuWitold <witekzawada@o2.pl>
2021-10-20 18:14:49 +0200

    static assets script

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,5 +1,6 @@
 import * as shell from 'shelljs';
 
 shell.cp('-R', 'src/dashboard/src/public/styles', 'dist/src/dashboard/src/public')
+shell.cp('-R', 'src/dashboard/src/public/styles', 'dist/src/dashboard/src/public')
 shell.cp('-R', 'src/dashboard/src/public/images', 'dist/src/dashboard/src/public')
 shell.cp('-R', 'src/dashboard/views', 'dist/src/dashboard')
